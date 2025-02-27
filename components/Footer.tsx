99a94773c0511ec049719263e707d6d61903287f
rohit <rohitsb96134@gmail.com>
2025-02-27 18:13:22 +0530

    Refactor Footer component for improved layout and styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,17 +18,13 @@ const socials = [
         href : "#",
         style: "text-muted-foreground hover:text-primary transition-colors duration-300"
     },
-    {
-        icon : <Instagram size={24} />,
-        href : "#",
-        style: "text-muted-foreground hover:text-primary transition-colors duration-300"
-    }
+   
 ]
 
 
 export const Footer = () => {
   return (
-    <footer className="py-12 border-t overflow-hidden relative">
+    <footer className="py-10 border-t overflow-hidden relative">
       {/* Gradient background */}
       <div className="absolute inset-0 bg-gradient-to-b from-transparent to-purple-500/5"></div>
       <div className="container mx-auto px-4 md:px-6  relative z-10">
@@ -41,22 +37,23 @@ export const Footer = () => {
             >
               Portfolio
             </Link>
-            <p
-             className="mt-2 text-sm text-muted-foreground"
-             >Creating beautiful web experiences</p>
+            <p className="mt-2 text-sm text-muted-foreground">
+              Creating beautiful web experiences
+            </p>
           </div>
           <div className="flex space-x-6">
-        {
-            socials.map((social, index) => (
-                <Link key={index} href={social.href}
-                 className={social.style}>{social.icon}</Link>
-       
-            ))
-        }
-
+            {socials.map((social, index) => (
+              <Link key={index} href={social.href} className={social.style}>
+                {social.icon}
+              </Link>
+            ))}
           </div>
         </div>
-        
+        <div className="mt-8  border-white/10 text-center">
+          <p className="text-muted-foreground text-center">
+            &copy; {new Date().getFullYear()} Portfolio. All rights reserved.
+          </p>
+        </div>
       </div>
     </footer>
   );
