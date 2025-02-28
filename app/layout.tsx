4114f6a1e7a439c4e9c5a95b66895554af68c43a
rohit <rohitsb96134@gmail.com>
2025-02-28 20:55:47 +0530

    fix: adjust Toaster component positioning in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
-        <Toaster position="center-top" />
+        <Toaster  />
       </body>
     </html>
   );
