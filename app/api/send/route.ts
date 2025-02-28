2fa8b4f213b4e4f400ff520d1c4e49b73321890d
rohit <rohitsb96134@gmail.com>
2025-02-28 20:59:19 +0530

    added resend route

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-// Initialize Resend with your API key
+
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
